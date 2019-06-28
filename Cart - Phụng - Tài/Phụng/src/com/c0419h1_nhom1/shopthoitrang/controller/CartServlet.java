6c940bfe6fc12a7efe0fdcdb2e34c2fbd68449ee
unknown <hoaiphungcntt@gmail.com>
2019-06-28 14:27:17 +0700

    sevlet

--- Cart - Phụng - Tài/Phụng/src/com/c0419h1_nhom1/shopthoitrang/controller/CartServlet.java
+++ Cart - Phụng - Tài/Phụng/src/com/c0419h1_nhom1/shopthoitrang/controller/CartServlet.java
@@ -1,6 +1,7 @@
 package com.c0419h1_nhom1.shopthoitrang.controller;
 
 import com.c0419h1_nhom1.shopthoitrang.entity.HoaDon;
+import com.c0419h1_nhom1.shopthoitrang.entity.SanPham;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,8 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "CartServlet", urlPatterns = "/cart")
 public class CartServlet extends HttpServlet {
@@ -28,6 +32,21 @@ public class CartServlet extends HttpServlet {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+                break;
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "createHoaDon":
+
+                break;
+            default:
+                ShowFormHoaDon(request, response);
                 break;
         }
     }
@@ -52,27 +71,17 @@ public class CartServlet extends HttpServlet {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if (action == null) {
-            action = "";
-        }
-        switch (action) {
-            case "createHoaDon":
-
-                break;
-            default:
-                ShowFormHoaDon(request, response);
-                break;
-        }
     }
 
     private void ShowFormHoaDon(HttpServletRequest request, HttpServletResponse response) {
-
-
+        HttpSession session = request.getSession();
+        List<SanPham> listSessionSanpham = (List<SanPham>) session.getAttribute("listSessionCartSanpham");
+        // đổ giỏ hàng qua cart
+        List<SanPham> newShowCart = new ArrayList<>();
+        for (int i = 0; i < listSessionSanpham.size(); i++) {
+            newShowCart.add(listSessionSanpham.get(i));
+        }
+        request.setAttribute("newShowCart", newShowCart); // đổ dữ liệu giỏ hàng qua cart
         RequestDispatcher dispatcher = request.getRequestDispatcher("Views/Cart/index.jsp");
         request.setAttribute("message", "New customer was created");
         try {
